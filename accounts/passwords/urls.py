84e446e47370adf3eaebf9348b370c023459b427
herobaby71 <thanh1998tom@gmail.com>
2018-01-29 12:10:09 -0500

    minor changes in url

--- accounts/passwords/urls.py
+++ accounts/passwords/urls.py
@@ -2,22 +2,22 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 urlpatterns  = [
-        url(r'^password/change/$',
+        url(r'^change/$',
                 auth_views.PasswordChangeView.as_view(),
                 name='password_change'),
-        url(r'^password/change/done/$',
+        url(r'^change/done/$',
                 auth_views.PasswordChangeDoneView.as_view(),
                 name='password_change_done'),
-        url(r'^password/reset/$',
+        url(r'^reset/$',
                 auth_views.PasswordResetView.as_view(),
                 name='password_reset'),
-        url(r'^password/reset/done/$',
+        url(r'^reset/done/$',
                 auth_views.PasswordResetDoneView.as_view(),
                 name='password_reset_done'),
-        url(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
                 auth_views.PasswordResetConfirmView.as_view(),
                 name='password_reset_confirm'),
-        url(r'^password/reset/complete/$',
+        url(r'^reset/complete/$',
                 auth_views.PasswordResetCompleteView.as_view(),
                 name='password_reset_complete'),
 ]
