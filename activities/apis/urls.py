2ac165a40ed12507a96a5ef2dacd9061c688e63e
herobaby71 <thanh1998tom@gmail.com>
2018-04-26 22:15:23 -0400

    fix minor typo in urls

--- activities/apis/urls.py
+++ activities/apis/urls.py
@@ -5,5 +5,5 @@ from .views import PokeAPIView, MessageAPIView, GetActivitiesAPIView
 urlpatterns = [
     url(r'^poke/$', PokeAPIView.as_view(), name='poke'),
     url(r'^message/$', MessageAPIView.as_view(), name='message'),
-    url(r'^get/activities/$', MessageAPIView.as_view(), name='get-activities'),
+    url(r'^get/activities/$', GetActivitiesAPIView.as_view(), name='get-activities'),
 ]
