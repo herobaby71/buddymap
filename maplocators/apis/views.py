fe48c8ffa5a8f1bf2739826ec5c0ce1faf5b1f6e
herobaby71 <thanh1998tom@gmail.com>
2018-04-27 01:46:40 -0400

    error

--- maplocators/apis/views.py
+++ maplocators/apis/views.py
@@ -26,20 +26,13 @@ class trackCurrentLocationAPIView(APIView):
 
 class postCurrentLocationAPIView(APIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = LocatorsSerializer
     def post(self, request, *args, **kwargs):
         data = request.data
-        print(data)
         user = request.user
-        try:
-            user.longitude = data.get("longitude")
-            user.latitude = data.get("latitude")
-            user.last_updated = datetime.now()
-            user.save()
-            locator = Locator.objects.create_locator(request.user, data.get("longitude"), data.get("latitude"))
-            locator.save()
-        except:
-            return Response({"success": False, 'error_message': "unidentified coordinate"}, status = HTTP_400_BAD_REQUEST)
+        user.longitude = data.get("longitude")
+        user.latitude = data.get("latitude")
+        user.last_updated = datetime.now()
+        user.save()
         return Response({"success": True}, status = HTTP_200_OK)
 
 class getFriendListLocationAPIView(APIView):
